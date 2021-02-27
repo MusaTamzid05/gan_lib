3f10e5f6cfefaeb34a25746357c3bec26ce1dcda
MusaTamzid05 <tamzid101@gmail.com>
2021-02-27 19:28:56 +0600

    generator added.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from simple_gan.models import build_discriminator
+#from simple_gan.models import build_discriminator
+from simple_gan.models import build_generator
 
 def main():
-    discriminator = build_discriminator(input_shape = (28, 28, 3))
-    print(discriminator.summary())
+    #discriminator = build_discriminator(input_shape = (28, 28, 3))
+    #print(discriminator.summary())
+    generator = build_generator(input_shape = (28, 28, 3))
+    generator.summary()
 
 
 if __name__ == "__main__":
--- simple_gan/models.py
+++ simple_gan/models.py
@@ -5,6 +5,8 @@ from tensorflow.keras.layers import Flatten
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Activation
 from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import Reshape
+from tensorflow.keras.layers import Conv2DTranspose
 
 
 
@@ -40,5 +42,40 @@ def build_discriminator(input_shape, alpha = 0.2):
     model.add(LeakyReLU(alpha = alpha))
     model.add(Dense(1))
     model.add(Activation("sigmoid"))
+    return model
+
+
+def generator_size_fixer(model, input_shape):
+    current_width = 28
+    target_width = input_shape[0]
+    while current_width < target_width:
+        model.add(Conv2DTranspose(input_shape[2], (5, 5), strides= (2, 2), padding = "same"))
+        current_width *= 2
+
+        if current_width != target_width:
+            model.add(Activation("relu"))
+    model.add(Activation("tanh"))
+    return model
+
+def build_generator(input_shape):
+    if input_shape[0] != input_shape[1]:
+        raise RuntimeError("input shape width and height must be same")
+
+    middle_layer_shape = (7, 7, 64)
+    model = Sequential()
+    model.add(Dense(input_dim = 500, units = 512))
+    model.add(Activation("relu"))
+    model.add(BatchNormalization())
+
+    model.add(Dense(middle_layer_shape[0] * middle_layer_shape[1] * middle_layer_shape[2]))
+    model.add(Activation("relu"))
+    model.add(BatchNormalization())
+
+    model.add(Reshape(middle_layer_shape))
+    model.add(Conv2DTranspose(32, (5, 5), strides= (2, 2), padding = "same"))
+    model.add(Activation("relu"))
+
+    model.add(Conv2DTranspose(input_shape[2], (5, 5), strides = (2, 2), padding = "same"))
+    model = generator_size_fixer(model = model, input_shape = input_shape)
     return model
 
