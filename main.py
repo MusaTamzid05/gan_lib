f850367b0ee53d7f8db15e5ff1bf525c1b1a8c7b
MusaTamzid05 <tamzid101@gmail.com>
2021-02-28 17:07:31 +0600

    reshaping the data.

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from simple_gan.data_loader import DataLoader
 
 def main():
 
-    data_loader = DataLoader(image_dir = "/home/musa/data/images/cat_and_dogs/dogs-vs-cats/train")
+    data_loader = DataLoader(image_dir = "./data", image_width = 224)
+    data_loader.load()
 
 
 if __name__ == "__main__":
--- simple_gan/data_loader.py
+++ simple_gan/data_loader.py
@@ -1,13 +1,15 @@
 import cv2
 import os
+import numpy as np
 
 class DataLoader:
 
-    def __init__(self, image_dir):
+    def __init__(self, image_dir, image_width):
 
         self.image_paths = self._load_paths(image_dir = image_dir)
         if len(self.image_paths) == 0:
             raise RuntimeError(f"No image in {self.image_paths}")
+        self.image_shape = (image_width, image_width)
 
     def _load_paths(self, image_dir):
         image_paths = [os.path.join(image_dir, image_name) for image_name in os.listdir(image_dir) if self._is_valid_image_file(image_name)]
@@ -21,3 +23,23 @@ class DataLoader:
                 return True
 
         return False
+
+    def load(self):
+
+        images = None
+
+        for image_path in self.image_paths:
+            image = cv2.imread(image_path)
+            image = cv2.resize(image, self.image_shape)
+
+            if images is None:
+                images = np.array(image)
+            else:
+                images = np.append(images, image)
+
+        images = images.reshape((len(self.image_paths), self.image_shape[0], self.image_shape[1], 3))
+        print(images.shape)
+
+
+
+
