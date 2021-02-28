0af65f4ebfd7f656c6a46dfd2108cbbaa6dad88d
MusaTamzid05 <tamzid101@gmail.com>
2021-02-28 17:40:09 +0600

    showing progress bar.

--- simple_gan/data_loader.py
+++ simple_gan/data_loader.py
@@ -30,8 +30,9 @@ class DataLoader:
     def load(self):
 
         images = None
+        len_images = len(self.image_paths)
 
-        for image_path in self.image_paths:
+        for index, image_path in enumerate(self.image_paths):
             image = cv2.imread(image_path)
             image = cv2.resize(image, self.image_shape)
 
@@ -39,9 +40,28 @@ class DataLoader:
                 images = np.array(image)
             else:
                 images = np.append(images, image)
+            self._show_progress(current_index = index, total = len_images )
 
         images = images.reshape((len(self.image_paths), self.image_shape[0], self.image_shape[1], 3))
         return images
+
+    def _show_progress(self, current_index, total):
+
+        current_index = current_index + 1
+        total = total
+
+        progress = int((current_index * 100 )/ total)
+        print("loading images => [", end = "")
+
+        for index in range(100):
+            if index   < progress:
+                print(">", end = "")
+            else:
+                print("-", end = "")
+
+        print(f"]({current_index} / {total})")
+
+
 
 
 
